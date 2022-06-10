e5501c0329dbfec8b5099b95de16b50d592bf0d9
Sharpie <78734695+wingdan@users.noreply.github.com>
2022-06-10 16:54:17 +0100

    Update parameters.py

--- parameters.py
+++ parameters.py
@@ -2,7 +2,7 @@ search_query = 'site:linkedin.com/in/ AND "Software Developer" AND "Manchester"'
 file_name = 'results_file.csv'
 pages = 20
 
-webdriver_location = "C:/Users/wingd/Desktop/WebScraping/chromedriver.exe"
+webdriver_location = "chromedriver.exe"
 
 
 linkedin_username = ''
